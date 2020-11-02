6a25959eb401c8c64cb6e8f56e70b17b78b7bf8e
Kezia Sulami <keziasulami.pooh@gmail.com>
2020-11-02 09:14:10 +0700

    Implement Issuing Tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -6,6 +6,7 @@ import "./DaiToken.sol";
 contract TokenFarm {
 
 	string public name = "Dapp Token Farm";
+	address public owner;
 	DappToken public dappToken;	// address
 	DaiToken public daiToken;	// address
 
@@ -17,10 +18,14 @@ contract TokenFarm {
 	constructor(DappToken _dappToken, DaiToken _daiToken) public {
 		dappToken = _dappToken;
 		daiToken = _daiToken;
+		owner = msg.sender;
 	}
 
-	// 1. Staking Tokens (Deposit)
+	// Staking Tokens (Deposit)
 	function stakeTokens(uint _amount) public {
+
+		// Require amount to be greater than 0
+		require(_amount > 0, "amount cannot be 0");
 
 		// Transfer Mock DAI tokens to this contract for staking
 		daiToken.transferFrom(msg.sender, address(this), _amount);
@@ -38,7 +43,21 @@ contract TokenFarm {
 		isStaking[msg.sender] = true;
 	}
 
-	// 2. Unstaking Tokens (Withdraw)
+	// Unstaking Tokens (Withdraw)
 
-	// 3. Issuing Tokens
+	// Issuing Tokens
+	function issueTokens() public {
+
+		// Only owner can call this function
+		require(msg.sender == owner, "caller must be the owner");
+
+		// Issue tokens to all stakers
+		for (uint i = 0; i < stakers.length; i++) {
+			address recipient = stakers[i];
+			uint balance = stakingBalance[recipient];
+			if (balance > 0) {
+				dappToken.transfer(recipient, balance);
+			}
+		}
+	}
 }
