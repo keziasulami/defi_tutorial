4711bccb1639dc26e0ebcb4bf950b96c74b0ce2d
Kezia Sulami <keziasulami.pooh@gmail.com>
2020-11-02 08:43:02 +0700

    Implement Staking Tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,6 +8,11 @@ contract TokenFarm {
 	string public name = "Dapp Token Farm";
 	DappToken public dappToken;	// address
 	DaiToken public daiToken;	// address
+
+	address[] public stakers;
+	mapping(address => uint) public stakingBalance;
+	mapping(address => bool) public hasStaked;
+	mapping(address => bool) public isStaking;
 	
 	constructor(DappToken _dappToken, DaiToken _daiToken) public {
 		dappToken = _dappToken;
@@ -15,6 +20,23 @@ contract TokenFarm {
 	}
 
 	// 1. Staking Tokens (Deposit)
+	function stakeTokens(uint _amount) public {
+
+		// Transfer Mock DAI tokens to this contract for staking
+		daiToken.transferFrom(msg.sender, address(this), _amount);
+
+		// Update staking balance
+		stakingBalance[msg.sender] += _amount;
+
+		// Add users to stakers array *only* if they haven't staked already
+		if (!hasStaked[msg.sender]) {
+			stakers.push(msg.sender);
+		}
+
+		// Update staking status
+		hasStaked[msg.sender] = true;
+		isStaking[msg.sender] = true;
+	}
 
 	// 2. Unstaking Tokens (Withdraw)
 
