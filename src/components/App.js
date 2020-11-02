a70191741c48adc3c086bf6dd2dc84edb7e9cd1f
Kezia Sulami <keziasulami.pooh@gmail.com>
2020-11-02 13:29:34 +0700

    Add Web3 + Metamask

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react'
+import Web3 from 'web3'
 import Navbar from './Navbar'
 import './App.css'
 
 class App extends Component {
+
+  // Connecting MetaMask
+
+  /*
+  React has these life cycle callbacks.
+  When the components are getting rendered, before it gets put on the page,
+  we can call the callback function and in this code, it'll call loadWeb3()
+  */
+
+  // this is a life cycle function inside of React
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3    // we have attached Web3 to the window at loadWeb3()
+
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts, "AAA")
+
+    this.setState({ account: accounts[0] })
+  }
+
+  // this function connects the app to the blockchain. this is a common pattern
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)               // attach Web3 to the window
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)   // attach Web3 to the window
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  // End of Connecting MetaMask
 
   constructor(props) {
     super(props)
