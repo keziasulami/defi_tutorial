21f153087683df302385eeecc113f8d3226d2151
Kezia Sulami <keziasulami.pooh@gmail.com>
2020-11-02 14:24:16 +0700

    Frontend: load data from blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
+import DappToken from '../abis/DappToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
 import Navbar from './Navbar'
 import './App.css'
 
@@ -19,6 +22,7 @@ class App extends Component {
     await this.loadBlockchainData()
   }
 
+  // this function fetches all of the data from the blockchain
   async loadBlockchainData() {
     const web3 = window.web3    // we have attached Web3 to the window at loadWeb3()
 
@@ -26,6 +30,59 @@ class App extends Component {
     console.log(accounts, "AAA")
 
     this.setState({ account: accounts[0] })
+
+    // detect the network inside of the app with Web3
+    const networkId = await web3.eth.net.getId()
+    console.log(networkId)  // 5777 is the network id for Ganache. see abis/
+
+    // hydrate the state = fill in all of the state variables
+
+    // load the Mock DAI Token from the abi file
+    const daiTokenData = DaiToken.networks[networkId]
+    if (daiTokenData) {
+
+      // create a JavaScript version of the smart contract with Web3
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+      this.setState({ daiToken })
+      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({ daiTokenBalance: daiTokenBalance.toString() })
+      console.log({ balance: daiTokenBalance })
+
+    } else {
+      window.alert('DaiToken contract has not been deployed to detected network')
+    }
+
+    // load the Dapp Token from the abi file
+    const dappTokenData = DappToken.networks[networkId]
+    if (dappTokenData) {
+
+      // create a JavaScript version of the smart contract with Web3
+      const dappToken = new web3.eth.Contract(DappToken.abi, dappTokenData.address)
+      this.setState({ dappToken })
+      let dappTokenBalance = await dappToken.methods.balanceOf(this.state.account).call()
+      this.setState({ dappTokenBalance: dappTokenBalance.toString() })
+      console.log({ balance: dappTokenBalance })
+
+    } else {
+      window.alert('DappToken contract has not been deployed to detected network')
+    }
+
+    // load the Token Farm from the abi file
+    const tokenFarmData = TokenFarm.networks[networkId]
+    if (tokenFarmData) {
+
+      // create a JavaScript version of the smart contract with Web3
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+      this.setState({ tokenFarm })
+      let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
+      this.setState({ stakingBalance: stakingBalance.toString() })
+      console.log({ balance: stakingBalance })
+
+    } else {
+      window.alert('TokenFarm contract has not been deployed to detected network')
+    }
+
+    this.setState({ loading: false })
   }
 
   // this function connects the app to the blockchain. this is a common pattern
@@ -47,7 +104,17 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0',
+
+      // smart contracts
+      daiToken: {},
+      dappToken: {},
+      tokenFarm: {},
+
+      daiTokenBalance: '0',
+      dappTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
     }
   }
 
