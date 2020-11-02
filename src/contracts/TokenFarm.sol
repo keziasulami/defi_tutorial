a4df7f5c4abd237940fe7b8c43f40c1b95c0c79c
Kezia Sulami <keziasulami.pooh@gmail.com>
2020-11-02 09:33:47 +0700

    Implement Unstaking Tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -44,6 +44,23 @@ contract TokenFarm {
 	}
 
 	// Unstaking Tokens (Withdraw)
+	function unstakeTokens() public {
+
+		// Fetch staking balance
+		uint balance = stakingBalance[msg.sender];
+
+		// Require balance to be greater than 0
+		require(balance > 0, "staking balance cannot be 0");
+
+		// Transfer Mock DAI tokens to the user
+		daiToken.transfer(msg.sender, balance);
+
+		// Reset staking balance
+		stakingBalance[msg.sender] = 0;
+
+		// Update staking status
+		isStaking[msg.sender] = false;
+	}
 
 	// Issuing Tokens
 	function issueTokens() public {
